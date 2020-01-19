6847c99feb4947047b34a22eb126d7dfc0e38fb9
ycdabao <ycdabao@yeah.net>
2020-01-20 00:08:53 +0800

    2020-1-9

--- src/main/java/com/imodou/blog/controller/PostController.java
+++ src/main/java/com/imodou/blog/controller/PostController.java
@@ -2,6 +2,7 @@ package com.imodou.blog.controller;
 
 import com.imodou.blog.common.constant.Message;
 import com.imodou.blog.common.entity.Result;
+import com.imodou.blog.utils.QiniuUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,39 +27,20 @@ public class PostController {
 
 
 
-/*
         @RequestMapping(value = "/upload" ,method = RequestMethod.POST)
         public Map editorMD(HttpServletRequest request, @RequestParam(value = "editormd-image-file", required = false) MultipartFile file) throws IOException {
             HashMap result = new HashMap();
 
             try {
-
-                //Calendar.getInstance()是获取一个Calendar对象并可以进行时间的计算，时区的指定
-                Calendar date = Calendar.getInstance();
-                //获得日期路径,MONTH个值的初始值是0，因此我们要用它来表示正确的月份时就需要加1。
-                File dateFile = new File(date.get(Calendar.YEAR)+"/"+(date.get(Calendar.MONTH)+1)+"/"+(date.get(Calendar.DATE)));
-                //获得文件最初的路径
                 String originalFile = file.getOriginalFilename();
                 String fileName= UUID.randomUUID().toString()+originalFile.substring(originalFile.lastIndexOf("."));
-                //得到完整路径名
-                File newFile = new File(uploadPath+File.separator+dateFile+File.separator+fileName);
 
-                */
-/*文件不存在就创建*//*
-
-                if(!newFile.getParentFile().exists()){
-                    newFile.getParentFile().mkdirs();
-                }
-                file.transferTo(newFile);
-                //System.out.println("/resources/upload/"+dateFile+File.separator+originalFile);
-               // System.out.println(dateFile+"/"+file.getOriginalFilename());
-                */
-/*JSON格式*//*
+                QiniuUtil.upload(file.getInputStream(),fileName);
 
 
                 result.put("success",1);
                 result.put("message",Message.FILE_UPLOAD_SUCCESS);
-                result.put("url","/upload/"+date.get(Calendar.YEAR)+"/"+(date.get(Calendar.MONTH)+1)+"/"+date.get(Calendar.DATE)+"/"+fileName);
+                result.put("url","http://cdn.imodou.com.cn/"+fileName);
             } catch (Exception e) {
 
                 result.put("success",0);
@@ -76,29 +58,12 @@ public class PostController {
 
         try {
 
-            //Calendar.getInstance()是获取一个Calendar对象并可以进行时间的计算，时区的指定
-            Calendar date = Calendar.getInstance();
-            //获得日期路径,MONTH个值的初始值是0，因此我们要用它来表示正确的月份时就需要加1。
-            File dateFile = new File(date.get(Calendar.YEAR)+"/"+(date.get(Calendar.MONTH)+1)+"/"+(date.get(Calendar.DATE)));
-            //获得文件最初的路径
             String originalFile = file.getOriginalFilename();
             String fileName= UUID.randomUUID().toString()+originalFile.substring(originalFile.lastIndexOf("."));
-            //得到完整路径名
-            File newFile = new File(uploadPath+File.separator+dateFile+File.separator+fileName);
 
-            */
-/*文件不存在就创建*//*
+            QiniuUtil.upload(file.getInputStream(),fileName);
 
-            if(!newFile.getParentFile().exists()){
-                newFile.getParentFile().mkdirs();
-            }
-            file.transferTo(newFile);
-            //System.out.println("/resources/upload/"+dateFile+File.separator+originalFile);
-            // System.out.println(dateFile+"/"+file.getOriginalFilename());
-            */
-/*JSON格式*//*
-
-          return new Result(true,Message.FILE_UPLOAD_SUCCESS,"/upload/"+date.get(Calendar.YEAR)+"/"+(date.get(Calendar.MONTH)+1)+"/"+date.get(Calendar.DATE)+"/"+fileName);
+          return new Result(true,Message.FILE_UPLOAD_SUCCESS,"http://cdn.imodou.com.cn/"+fileName);
 
 
         } catch (Exception e) {
@@ -108,7 +73,6 @@ public class PostController {
         }
 
     }
-*/
 
 
 
